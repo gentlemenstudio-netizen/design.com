0f5259d79df4b0616f20024553a5445549be45b5
gentlemenstudio-netizen <gentlemenstudio2@gmail.com>
2025-12-30 12:28:55 +0530

    RollBack Uploadthing

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,20 +1,22 @@
-export const runtime = "nodejs";
-
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+
 import { auth } from "@/auth";
-
+ 
 const f = createUploadthing();
-
+ 
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    .middleware(async () => {      
-
-      return { userId: '19d68088-f81e-4351-b238-e580edc4a71a' };
+    .middleware(async ({ req }) => {
+      const session = await auth();
+ 
+      if (!session) throw new UploadThingError("Unauthorized");
+ 
+      return { userId: session.user?.id };
     })
-    .onUploadComplete(async ({ file }) => {
+    .onUploadComplete(async ({ metadata, file }) => {
       return { url: file.url };
     }),
 } satisfies FileRouter;
-
+ 
 export type OurFileRouter = typeof ourFileRouter;
