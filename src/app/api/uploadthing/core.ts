141d25403324e4f24968198cdaad3d2b2da45a35
gentlemenstudio-netizen <gentlemenstudio2@gmail.com>
2025-12-30 12:25:55 +0530

    Testing Uploading

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -10,7 +10,7 @@ export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     .middleware(async () => {      
 
-      return { userId: '19d68088-f81e-4351-b238-e580edc4a71a };
+      return { userId: '19d68088-f81e-4351-b238-e580edc4a71a' };
     })
     .onUploadComplete(async ({ file }) => {
       return { url: file.url };
